a461eb5bfcaa1fd5a36fc6aeb0c4bbf0c5103b70
kathleen7184 <acostakathleenjean808@gmail.com>
2026-03-24 12:02:50 +0800

    header kag footer a

---
+++ app/Livewire/Pages/Header/PublicHeader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages\Header;
+
+use Livewire\Component;
+
+class PublicHeader extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.header.public-header');
+    }
+}
